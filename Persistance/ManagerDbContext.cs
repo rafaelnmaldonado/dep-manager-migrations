1010a009cbc1d0d513eb08a925cd3ce78d9a9611
Rafael <rafaelnuttimaldonado@gmail.com>
2023-03-30 15:09:34 -0300

    Add project files.

---
+++ Persistance/ManagerDbContext.cs
@@ -0,0 +1,39 @@
+﻿using dep_manager_singleton.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace dep_manager_singleton.Persistance
+{
+    public class ManagerDbContext : DbContext
+    {
+        public ManagerDbContext(DbContextOptions<ManagerDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Department>(e =>
+            {
+                e.HasKey(dep => dep.Id);
+
+                e.Property(dep => dep.Name)
+                    .IsRequired(false)
+                    .HasColumnType("VARCHAR(255)");
+
+                e.Property(dep => dep.Acronym)
+                    .IsRequired(false)
+                    .HasColumnType("VARCHAR(7)");
+            });
+
+            builder.Entity<Employee>(e =>
+            {
+                e.HasKey(emp => emp.Id);
+            });
+        }
+
+    }
+}
